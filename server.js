b26d000b36f2ec2cbef97382931f30601ee7abcc
yuvraj <yuvrajsingh2348q@gmail.com>
2023-04-04 12:51:04 +0530

    fix cors error

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const connectDb = require("./config/db");
-const cors = require("cors");
 require("dotenv").config();
 
 const isAuthenticated = require("./middlewares/auth");
